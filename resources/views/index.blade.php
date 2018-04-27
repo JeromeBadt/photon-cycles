d35d6cd1db6fa564ba2518e05cfbf0598a672de2
Jerome Badt <jerome.badt@gmail.com>
2018-04-27 03:51:42 +0200

    Rename base view and add bundles + app-root element

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <title>Photon Cycles</title>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+        </style>
+    </head>
+    <body>
+        <app-root></app-root>
+        <script src="{{ mix('build/js/polyfills.js') }}"></script>
+        <script src="{{ mix('build/js/main.js') }}"></script>
+    </body>
+</html>

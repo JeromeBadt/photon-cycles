d734689e30c7f76f75817fe979875448ab0fb565
Jerome Badt <jerome.badt@gmail.com>
2018-04-27 03:47:13 +0200

    Update webpack config - Use laravel mix - Bundle polyfills - Add ContextReplacementPlugin to provide context for Angular's use of System.import     - see https://github.com/angular/angular/issues/11580#issuecomment-327338189

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,8 @@
-module.exports = {
-    entry: 'resources/assets/js/main.ts',
-    output: {
-        filename: 'bundle.js',
-        path: 'public/js'
-    },
+const mix = require('laravel-mix');
+const webpack = require('webpack');
+const path = require('path');
+
+mix.webpackConfig({
     module: {
         rules: [
             {
@@ -14,6 +13,15 @@ module.exports = {
         ]
     },
     resolve: {
-        extensions: [".tsx", ".ts", ".js"]
-    }
-};
+        extensions: ["*", ".js", ".ts"]
+    },
+    plugins: [
+        new webpack.ContextReplacementPlugin(
+            /(.+)?angular([\\/])core(.+)?/,
+            path.join(__dirname, 'resources/assets/js')
+        ),
+    ]
+});
+
+mix.js('resources/assets/js/main.ts', 'public/build/js').sourceMaps().version();
+mix.js('resources/assets/js/polyfills.ts', 'public/build/js').sourceMaps().version();

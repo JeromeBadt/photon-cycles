deff2c74fccda500f822fdefddc92224d538c885
JeromeBadt <jerome.badt@gmail.com>
2018-08-16 19:20:27 +0200

    Add JWT config

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    /**
+     * Get a JWT via given credentials.
+     */
+    public function login(): JsonResponse
+    {
+        $credentials = request(['email', 'password']);
+
+        if (!$token = Auth::attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Get the authenticated User.
+     */
+    public function me(): JsonResponse
+    {
+        return response()->json(Auth::user());
+    }
+
+    public function logout(): JsonResponse
+    {
+        Auth::logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh(): JsonResponse
+    {
+        return $this->respondWithToken(Auth::refresh());
+    }
+
+    protected function respondWithToken(string $token): JsonResponse
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::factory()->getTTL() * 60,
+            'user' => Auth::user()->name
+        ]);
+    }
+}

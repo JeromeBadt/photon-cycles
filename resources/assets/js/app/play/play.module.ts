10ef021d6711410a4538c3021bc40579e9664570
ManuelRuss <mr134@hdm-stuttgart.de>
2018-08-16 22:02:13 +0200

    Update component structure Update routing layout

--- resources/assets/js/app/play/play.module.ts
+++ resources/assets/js/app/play/play.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PlayComponent } from './play.component';
+import { RouterModule } from "@angular/router";
 
 @NgModule({
     declarations: [
         PlayComponent
     ],
-    imports: [],
+    imports: [RouterModule],
     exports: [
         PlayComponent
     ]
